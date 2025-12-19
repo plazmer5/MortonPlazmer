9808e20162f37b3753663b2085dc46940b084b92
Dmitry Plusmer <95934763+plazmer5@users.noreply.github.com>
2025-12-20 00:07:36 +0300

    Update CustomWebViewHandler.cs

--- MortonPlazmer/Platforms/iOS/CustomWebViewHandler.cs
+++ MortonPlazmer/Platforms/iOS/CustomWebViewHandler.cs
@@ -1,10 +1,6 @@
-﻿#nullable disable
-
-using WebKit;
 using Microsoft.Maui.Handlers;
-using MortonPlazmer.Controls;
+using WebKit;
 using Foundation;
-using CoreGraphics;
 
 namespace MortonPlazmer.Platforms.iOS
 {
@@ -12,48 +8,27 @@ namespace MortonPlazmer.Platforms.iOS
     {
         protected override WKWebView CreatePlatformView()
         {
-            // Используем WKWebPagePreferences вместо устаревших WKPreferences
-            var pagePreferences = new WKWebpagePreferences
-            {
-                AllowsContentJavaScript = true // включаем JS безопасным современным способом
-            };
-
             var config = new WKWebViewConfiguration
             {
-                DefaultWebpagePreferences = pagePreferences,
-                AllowsInlineMediaPlayback = true,
-                WebsiteDataStore = WKWebsiteDataStore.DefaultDataStore,
-                UserContentController = new WKUserContentController()
+                Preferences = new WKPreferences
+                {
+                    JavaScriptEnabled = true
+                }
             };
 
-            // Добавление viewport meta через JS
-            var js = @"
-                var meta = document.createElement('meta');
-                meta.name = 'viewport';
-                meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes';
-                document.getElementsByTagName('head')[0].appendChild(meta);
-            ";
-            var userScript = new WKUserScript(new NSString(js), WKUserScriptInjectionTime.AtDocumentEnd, true);
-            config.UserContentController.AddUserScript(userScript);
+            var webView = new WKWebView(
+                CoreGraphics.CGRect.Empty,
+                config);
 
-            return new WKWebView(CGRect.Empty, config);
+            return webView;
         }
 
         protected override void ConnectHandler(WKWebView platformView)
         {
             base.ConnectHandler(platformView);
 
-            // Настройка масштабирования (аналог UseWideViewPort)
-            platformView.ScrollView.BouncesZoom = true;
-            platformView.ScrollView.MaximumZoomScale = 5f;
-            platformView.ScrollView.MinimumZoomScale = 1f;
-            platformView.ScrollView.ZoomScale = 1f;
-            platformView.ScrollView.PinchGestureRecognizer.Enabled = true;
-
-            // Разрешение навигации жестами (свайп назад/вперед)
-            platformView.AllowsBackForwardNavigationGestures = true;
+            platformView.NavigationDelegate =
+                new IOSWebViewNavigationDelegate();
         }
     }
 }
-
-#nullable restore

45330a0f14743a9cc6c2a7728507b26fd98e410c
Dmitry Plusmer <95934763+plazmer5@users.noreply.github.com>
2025-12-19 23:53:19 +0300

    Update MainActivity.cs

--- MortonPlazmer/Platforms/Android/MainActivity.cs
+++ MortonPlazmer/Platforms/Android/MainActivity.cs
@@ -1,27 +1,22 @@
-﻿using Android.App;
-using Android.Content;
+using Android.App;
 using Android.Content.PM;
-using Android.OS;
-using Android.Runtime;
-using Android.Webkit;
-using MortonPlazmer.Platforms.Android;
+using Microsoft.Maui;
 
 namespace MortonPlazmer
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public partial class MainActivity : MauiAppCompatActivity
+    [Activity(
+        Theme = "@style/Maui.SplashTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges =
+            ConfigChanges.ScreenSize |
+            ConfigChanges.Orientation |
+            ConfigChanges.UiMode |
+            ConfigChanges.ScreenLayout |
+            ConfigChanges.SmallestScreenSize |
+            ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
     {
-        public UniversalDownloadListener? DownloadListener { get; set; }
-
-        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent? data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-
-            if (requestCode == 1001 && resultCode == Result.Ok && data?.Data != null)
-            {
-                DownloadListener?.OnFileSelected(data.Data);
-            }
-        }
     }
-
 }
+
